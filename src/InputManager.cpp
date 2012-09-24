1663835aabb69d88289cd9ee887f0371d3675d9e
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-09-25 00:38:12 +0200

    Wrote removeInputListener in InputManager.
    Did some minor changes as well.

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -20,11 +20,11 @@ InputManager::~InputManager()
 
 void InputManager::passInputEvent(InputType input, float locX, float locY)
 {
+    Log(LOG_EVENT, "InputManager", "Passing inputEvent");
+    
     for (int i = 0; i < m_inputArray.m_size; i++) {
         m_inputArray.m_listeners[i]->didRecieveInputEvent(input, locX, locY);
     }
-    
-    Log(LOG_EVENT, "InputManager", "Passing inputEvent");
 }
 void InputManager::addInputListener(IInputListener* listener)
 {
@@ -38,10 +38,39 @@ void InputManager::addInputListener(IInputListener* listener)
     m_inputArray.m_listeners = newArray;
     m_inputArray.m_size++;
 }
-
+bool InputManager::removeInputListener(IInputListener* listener)
+{
+    if (m_inputArray.m_size <= 0)
+        return false;
+    
+    int r_index = -1;
+    for (int i = 0; i < m_inputArray.m_size; i++) {
+        if (m_inputArray.m_listeners[i] == listener) {
+            r_index = i;
+            break;
+        }
+    }
+    
+    if (r_index == -1)
+        return false;
+    
+    IInputListener** newArray = new IInputListener*[m_inputArray.m_size - 1];
+    for (int i = 0; i < m_inputArray.m_size - 1; i++) {
+        if (i < r_index)
+            newArray[i] = m_inputArray.m_listeners[i];
+        else if (i > r_index)
+            newArray[i] = m_inputArray.m_listeners[i+1];
+    }
+    m_inputArray.m_listeners = newArray;
+    m_inputArray.m_size--;
+    
+    return true;
+}
 //////////////////////////////////////
 //  Static part of implementation   //
 //////////////////////////////////////
+
+static InputManager* s_sharedManager;
 
 InputManager* InputManager::getSharedManager()
 {
