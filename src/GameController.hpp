8748d237d4adb4d6d82181fd6f01554c9d825811
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-10-01 01:17:31 +0200

    Made GameController override IInputListener correctly.
    Also added resource path as a parameter in the constructor.

--- src/GameController.hpp
+++ src/GameController.hpp
@@ -12,17 +12,20 @@
 #include "GLRenderer.hpp"
 #include "FileManager.hpp"
 #include "IInputListener.hpp"
+#include "EInputType.hpp"
 
 class GameController : public IInputListener {
 private:
     GLRenderer* m_renderer;
     FileManager* m_fileManager;
 public:
-    GameController(int width, int height);
+    GameController(int width, int height, const char* resourcesPath);
     ~GameController();
 
     void setRenderer(GLRenderer* r);
     GLRenderer* getRenderer();
+    
+    virtual void didRecieveInputEvent(InputType type, int locX, int locY);
 };
 
 #endif
