b84f19f44f26be916666cd90cd95788db1746169
Z3B0 <odbjer@student.chalmers.se>
2012-09-30 15:17:16 +0200

    Finished implementing loadTextureFromFile function in Filemanager

--- src/FileManager.cpp
+++ src/FileManager.cpp
@@ -12,25 +12,34 @@
 
 FileManager::FileManager(const char* basePath)
 {
-
+	m_basePath = basePath;
 }
 FileManager::~FileManager()
 {
 
 }
 
-CLTexture* loadTextureFromFile(const char* file)
+CLTexture* loadTextureFromFile(const char* filename)
 {
+
+	CLTexture texture;
+	const char filepath = m_basePath + filename;
+	Log(LOG_ERROR, "FileManager", "FilePath: " + filepath);
+
 	//Decode the requested file
-	std::vector<unsigned char> image;
-	unsigned width;
-	unsigned height;
-	unsigned error = Lodepng::decode(image, width, height, file);
+	//Decodes it in a way that it is acceptable to assume it has RGBA format.
+	unsigned error = decoder.decode(texture.data, texture.width, texture.height, filepath);
 
 	if (error != 0)
 	{
 		Log(LOG_ERROR, "FileManager", "Error when trying to load file");
 		return 1;
 	}
+
+	//Set the format and the type
+	texture.type = GL_UNSIGNED_BYTE;
+	texture.internalformat = GL_RGBA;
+
+	return texture;
 
 }
