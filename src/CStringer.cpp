5e57e56afbc61e41e25865cef76bb3d8ee492699
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-10-01 21:32:05 +0200

    Created a helper function to create CStrings.
    This is very useful when debugging.

---
+++ src/CStringer.cpp
@@ -0,0 +1,53 @@
+//
+//  CStringer.cpp
+//  CatchiOS
+//
+//  Created by John Barbero Unenge on 10/1/12.
+//  Copyright (c) 2012 John Barbero Unenge. All rights reserved.
+//
+
+#include "CStringer.hpp"
+
+#include <sstream>
+#include <iostream>
+#include <string>
+
+int s_overhead = 50;
+int s_incrementSize = 50;
+
+const char* generateCString(const char* format, ...)
+{
+    std::ostringstream message;
+    
+    va_list ap;
+    va_start(ap, format);
+    
+    const char* fCpy = format;
+    
+    while (*fCpy) {
+        if (*fCpy == '%') {
+            fCpy++;
+            switch (*fCpy) {
+                case 's':
+                    message << va_arg(ap, const char*);
+                    break;
+                    
+                case 'i':
+                    message << va_arg(ap, int);
+                    break;
+                    
+                case 'd':
+                    message << va_arg(ap, double);
+                    break;
+            }
+        } else {
+            message << *fCpy;
+        }
+        
+        fCpy++;
+    }
+    
+    va_end(ap);
+    
+    return message.str().c_str();
+}
---
+++ src/CStringer.hpp
@@ -0,0 +1,14 @@
+//
+//  CStringer.h
+//  CatchiOS
+//
+//  Created by John Barbero Unenge on 10/1/12.
+//  Copyright (c) 2012 John Barbero Unenge. All rights reserved.
+//
+
+#ifndef __CatchiOS__CStringer__
+#define __CatchiOS__CStringer__
+
+const char* generateCString(const char* format, ...);
+
+#endif /* defined(__CatchiOS__CStringer__) */
