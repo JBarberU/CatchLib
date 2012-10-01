d467ff6a590827ed12d6e0b0b79c4008183c2b8f
Jesper Persson <JesperT.Persson@gmail.com>
2012-10-01 16:50:30 +0200

    PlatformPoint changed to PlatformBlock.

---
+++ src/GameModel/Map/PlatformBlock.hpp
@@ -0,0 +1,19 @@
+//
+//  PlatformBlock.hpp
+//  CatchLib
+//
+//  Created by Jesper Persson on 9/29/12.
+//  Copyright (c) 2012 Jesper Persson. All rights reserved.
+//
+//	A class for representing a block in a Platform.
+//
+//
+
+#include "PBody.hpp"
+
+class PlatformBlock: public PBody
+{
+public:
+	~PlatformBlock();
+	PlatformBlock();
+};
