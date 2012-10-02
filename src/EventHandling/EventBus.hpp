182b7240ffd23a358a7fb11ca7481b1d99be2767
Z3B0 <odbjer@student.chalmers.se>
2012-10-02 18:47:56 +0200

    Added documentation for our structure

--- src/EventHandling/EventBus.hpp
+++ src/EventHandling/EventBus.hpp
@@ -8,6 +8,16 @@
 #include "EEvent.hpp"
 #include "IEventListener.hpp"
 
+/*
+ * Structure for organizing our array of eventListeners.
+ * m_eventListeners --  our array of eventListeners.
+ * m_size -- 			the size of our array.
+ * m_incrementBy -- 	how much the size of the array should be incremented
+ * 						by whenever m_index == m_size.
+ * m_index -- 			the index of the last element of the array, it is set to -1
+ * 			  			as default to prevent events to be performed when the array
+ * 			  			is empty.
+ */
 struct EventListenerArray
 {
 	IEventListener** m_eventListeners;
@@ -17,7 +27,7 @@ struct EventListenerArray
 	EventListenerArray()
 	{
 		m_incrementBy = 5;
-		m_index = 0;
+		m_index = -1;
 		m_size = 0;
 		m_eventListeners = 0;
 	}
