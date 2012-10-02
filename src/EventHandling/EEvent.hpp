97b7725db009ba02afa3b34bfc46f4a040eebdf4
Z3B0 <odbjer@student.chalmers.se>
2012-10-02 16:02:34 +0200

    Added Enum class for events

---
+++ src/EventHandling/EEvent.hpp
@@ -0,0 +1,20 @@
+//
+//  EEvent.hpp
+//  CatchLib
+//
+//  Created by Jesper Persson and Sebastian Odbjer on 2/10-12.
+//  Copyright (c) 2012 Catch22. All rights reserved.
+//
+//	Enum class for different events.
+//
+//
+
+enum EEvent
+{
+	DID_JUMP,
+	DID_COLLIDE,
+	DID_THROW,
+	GAME_STARTED,
+	GAME_PAUSED,
+	GAME_OVER
+};
