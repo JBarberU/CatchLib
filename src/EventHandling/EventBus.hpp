4fee3849117056c0b8e045cbdd35280a893248bf
Jesper Persson <JesperT.Persson@gmail.com>
2012-10-02 18:08:01 +0200

    added struct EventListenerArray

--- src/EventHandling/EventBus.hpp
+++ src/EventHandling/EventBus.hpp
@@ -1,4 +1,3 @@
-
 //
 //  EventBus.hpp
 //  CatchLib
@@ -6,15 +5,33 @@
 //  Created by Jesper Persson and Sebastian Odbjer on 2/10-12.
 //  Copyright (c) 2012 Catch22. All rights reserved.
 //
+#include "EEvent.hpp"
+#include "IEventListener.hpp"
+
+struct EventListenerArray
+{
+	IEventListener** m_eventListeners;
+	int m_size;
+	int m_incrementBy;
+	int m_index;
+	EventListenerArray()
+	{
+		m_incrementBy = 5;
+		m_index = 0;
+		m_size = 0;
+		m_eventListeners = 0;
+	}
+};
 
 class EventBus
 {
 public:
-	static EventBus getSharedInstance();
-	void publishEvent();
-	void addEventHandler();
-	void removeEventHandler();
+	static EventBus* getSharedInstance();
+	void publishEvent(EEvent event, void* source);
+	void addEventListener(IEventListener* listener);
+	void removeEventListener(IEventListener* listener);
 private:
+	EventListenerArray* m_listeners;
 	~EventBus();
 	EventBus();
 };
