538d19ec89c88e1e05a3f4f4039ed85d2e7b6a91
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-10-08 01:45:22 +0200

    Created a player class.
    This will be used to track a PBody and has some other special
    features such as a convenience function for jumping.

---
+++ src/GameModel/Entities/Player.cpp
@@ -0,0 +1,29 @@
+//
+//  Player.cpp
+//  CatchiOS
+//
+//  Created by John Barbero Unenge on 10/7/12.
+//  Copyright (c) 2012 John Barbero Unenge. All rights reserved.
+//
+
+#include "Player.hpp"
+#include "../../EventHandling/EventBus.hpp"
+#include "../Physics/Vector2d.hpp"
+#include "../../Helper/Logger.hpp"
+
+Vector2d* JUMP = new Vector2d(0, 450);
+
+Player::Player()
+{
+    m_body = new PBody(true, new Vector2d(100, 100), new Vector2d(32, 32), new Vector2d(0, 0));
+    EventBus::getSharedInstance()->publishEvent(PBODY_CREATED, m_body);
+}
+Player::~Player()
+{
+    
+}
+void Player::jump()
+{
+    m_body->addVector(JUMP);
+    Log(LOG_INFO, "Player", generateCString("PlayerY: %d", m_body->m_movement->m_y));
+}
