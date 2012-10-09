010ff8ed5df7a21b733243f42064d26ec62a6432
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-10-09 15:02:58 +0200

    Wrote a logger for use when compiling tests that won't run with NDK.

---
+++ tests/Logger.cpp
@@ -0,0 +1,31 @@
+/*
+ * Logger.cpp
+ *
+ *  Created on: Oct 9, 2012
+ *      Author: Barber
+ */
+
+#include "../src/Helper/Logger.hpp"
+#include <iostream>
+
+void Log(LogTag tag, const char* title, const char* message)
+{
+    const char* tag_str;
+
+    switch (tag) {
+        case LOG_INFO:
+            tag_str = "LOG_INFO";
+            break;
+        case LOG_EVENT:
+            tag_str = "LOG_EVENT";
+            break;
+        case LOG_ERROR:
+            tag_str = ">>>> LOG_ERROR <<<<";
+            break;
+        default:
+            tag_str = "UNKNOWN_LOG_TAG";
+            break;
+    }
+
+    std::cout << tag_str << "\t" << title << "\t" << message << "\n";
+}
