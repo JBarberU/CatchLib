c41cd7ae627281271d8bfe514f76819594663cda
Z3B0 <odbjer@student.chalmers.se>
2012-10-09 19:52:08 +0200

    Changed some comments

--- src/GameModel/Map/Platform.hpp
+++ src/GameModel/Map/Platform.hpp
@@ -1,7 +1,7 @@
 //
 //  File:       Platform.hpp
 //  Class:      Platform
-//  Author:     Jesper Persson
+//  Author:     Sebastian Odbjer
 //              All code is my own except where credited to others.
 //
 //	Copyright (c) 2012 by Catch22. All Rights Reserved.
