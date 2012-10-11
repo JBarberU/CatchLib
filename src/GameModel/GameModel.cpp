4c506fd0dfbacdb6a6be097aa065f17d1e3c272c
Jesper Persson <JesperT.Persson@gmail.com>
2012-10-11 18:02:23 +0200

    Added function to throwChainsawAt and added another PBody to jump over.

--- src/GameModel/GameModel.cpp
+++ src/GameModel/GameModel.cpp
@@ -16,8 +16,10 @@ GameModel::GameModel()
 //    m_gameMap = new GameMap();
     
     PBody* body = new PBody(new Vector2d(0, 0), new Vector2d(480, 50), false, true, false, PB_PLATFORM);
+    PBody* body2 = new PBody(new Vector2d(240, 51), new Vector2d(20, 50), false, true, false, PB_PLATFORM);
     
     EventBus::getSharedInstance()->publishEvent(PBODY_CREATED, (PBody *)body);
+    EventBus::getSharedInstance()->publishEvent(PBODY_CREATED, (PBody *)body2);
     
     m_player = new Player();
     Log(LOG_INFO, "GameModel", "GameModel has been constructed.");
@@ -35,4 +37,8 @@ void GameModel::update(float dt)
 void GameModel::playerJump()
 {
     m_player->jump();
+}
+void GameModel::playerThrowAt(int x, int y)
+{
+    Log(LOG_INFO, "GameModel", generateCString("Player threw his chainsaw at: %i,%i", x, y));
 }
