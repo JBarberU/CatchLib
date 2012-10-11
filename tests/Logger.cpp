59c0c653ba24edf03f0b339f52538da9a4736d77
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-10-11 02:17:37 +0200

    Implemented a test logger.
    This should be commented out when not needed.

--- tests/Logger.cpp
+++ tests/Logger.cpp
@@ -10,5 +10,22 @@
 
 void Log(LogTag tag, const char* title, const char* message)
 {
-    // Doing nothing here on purpose
+    const char* tag_str;
+    
+    switch (tag) {
+        case LOG_INFO:
+            tag_str = "LOG_INFO";
+            break;
+        case LOG_EVENT:
+            tag_str = "LOG_EVENT";
+            break;
+        case LOG_ERROR:
+            tag_str = ">>>> LOG_ERROR <<<<";
+            break;
+        default:
+            tag_str = "UNKNOWN_LOG_TAG";
+            break;
+    }
+    
+    std::cout << tag_str << "\t" << title << "\t" << message << "\n";
 }
