ad47fa7d6940acf50a7657fc609c79ebd12635bf
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-10-11 02:20:27 +0200

    Rewrote the whole class to use single axis theorem for collision.
    It uses one vector for each corner of the polygon which would allow for
    rotations and collisions between more complex bodies.

--- src/GameModel/Physics/PBody.cpp
+++ src/GameModel/Physics/PBody.cpp
@@ -11,83 +11,176 @@
 #include "PBody.hpp"
 #include "../../Helper/Logger.hpp"
 
-PBody::PBody(bool affectedByGravity, bool isStationary, Vector2d* position, Vector2d* size, Vector2d* movement)
+PBody::PBody(Vector2dArray* vectorArray, Vector2d* centerOfMassOffset, bool affectedByGravity, bool stationaryObject, bool rotatableObject, PBodyType tag)
 {
-    m_position = position;
-    m_size = size;
-    m_movement = movement;
+    m_vectorArray = vectorArray;
+    m_centerOfMassOffset = centerOfMassOffset;
     m_affectedByGravity = affectedByGravity;
-    m_stationary = isStationary;
+    m_stationaryObject = stationaryObject;
+    m_rotatableObject = rotatableObject;
+    
+    m_movementVector = new Vector2d(0,0);
+    m_tag = tag;
 }
+PBody::PBody(Vector2dArray* vectorArray, bool affectedByGravity, bool stationaryObject, bool rotatableObject, PBodyType tag)
+{
+    m_vectorArray = vectorArray;
+    m_centerOfMassOffset = new Vector2d(0,0);
+    m_affectedByGravity = affectedByGravity;
+    m_stationaryObject = stationaryObject;
+    m_rotatableObject = rotatableObject;
+    
+    m_movementVector = new Vector2d(0,0);
+    m_tag = tag;
+}
+PBody::PBody(Vector2dArray* vectorArray, bool stationaryObject, PBodyType tag)
+{
+    m_vectorArray = vectorArray;
+    m_centerOfMassOffset = new Vector2d(0,0);
+    m_affectedByGravity = !stationaryObject;
+    m_stationaryObject = stationaryObject;
+    m_rotatableObject = !stationaryObject;
+    
+    m_movementVector = new Vector2d(0,0);
+    m_tag = tag;
+}
+PBody::PBody(Vector2d* position, Vector2d* size, bool affectedByGravity, bool stationaryObject, bool rotatableObject, PBodyType tag)
+{
+    Vector2d** vArr = new Vector2d*[4];
+    vArr[0] = new Vector2d(position->m_x, position->m_y);
+    vArr[1] = new Vector2d(position->m_x + size->m_x, position->m_y);
+    vArr[2] = new Vector2d(position->m_x, position->m_y + size->m_y);
+    vArr[3] = new Vector2d(position->m_x + size->m_x, position->m_y + size->m_y);
+    
+    Log(LOG_INFO, "PBody", generateCString("vArr3: %d", vArr[3]->m_x));
+    
+    m_vectorArray = new Vector2dArray(vArr, 4);
+    m_centerOfMassOffset = new Vector2d(0,0);
+    m_affectedByGravity = affectedByGravity;
+    m_stationaryObject = stationaryObject;
+    m_rotatableObject = rotatableObject;
+    
+    m_movementVector = new Vector2d(0,0);
+    m_tag = tag;
+}
+
 PBody::~PBody()
 {
-    delete m_position;
-    delete m_size;
-    delete m_movement;
+
+    delete m_centerOfMassOffset;
+    delete m_movementVector;
+    delete m_vectorArray;
 }
 
 void PBody::applyForce(float dt)
 {
-    *m_position += (*m_movement * dt);
+    for (int i = 0; i < m_vectorArray->m_size; i++) {
+        m_vectorArray->m_vectors[i]->m_x += (m_movementVector->m_x * dt);
+        m_vectorArray->m_vectors[i]->m_y += (m_movementVector->m_y * dt);
+    }
 }
 void PBody::revertForce(float dt)
 {
-    *m_position -= (*m_movement * dt);
+    for (int i = 0; i < m_vectorArray->m_size; i++) {
+        m_vectorArray->m_vectors[i]->m_x -= (m_movementVector->m_x * dt);
+        m_vectorArray->m_vectors[i]->m_y -= (m_movementVector->m_y * dt);
+    }
+}
+void PBody::applyForceWithMask(Vector2d* mask, float dt)
+{
+    for (int i = 0; i < m_vectorArray->m_size; i++) {
+        m_vectorArray->m_vectors[i]->m_x += ((m_movementVector->m_x * mask->m_x) * dt);
+        m_vectorArray->m_vectors[i]->m_y += ((m_movementVector->m_y * mask->m_y) * dt);
+    }
+}
+void PBody::revertForceWithMask(Vector2d* mask, float dt)
+{
+    for (int i = 0; i < m_vectorArray->m_size; i++) {
+        m_vectorArray->m_vectors[i]->m_x -= ((m_movementVector->m_x * mask->m_x) * dt);
+        m_vectorArray->m_vectors[i]->m_y -= ((m_movementVector->m_y * mask->m_y) * dt);
+    }
 }
 void PBody::addVector(Vector2d* vector)
 {
-    *m_movement += *vector;
-    Log(LOG_INFO, "PBody", generateCString("AddingvectorY: %d Movement: %d", vector->m_y, m_movement->m_y));
+    *m_movementVector += *vector;
 }
 void PBody::removeVector(Vector2d* vector)
 {
-    *m_movement -= *vector;
-    Log(LOG_INFO, "PBody", generateCString("Removing vectorY: %d Movement: %d", vector->m_y, m_movement->m_y));
+    *m_movementVector -= vector;
 }
 void PBody::resetMovementVector()
 {
-    this->m_movement->zap();
+    m_movementVector = new Vector2d(0,0);
+}
+void PBody::maskMovementVector(float x, float y)
+{
+    this->m_movementVector->m_x *= x;
+    this->m_movementVector->m_y *= y;
+}
+void PBody::rotateAround(Vector2d* pivotPoint, float degrees)
+{
+    // Implement me plxzz
+}
+void PBody::translateBy(Vector2d* vector)
+{
+    // Implement me plxzz
 }
 
-Vector2d* PBody::getPosition()
+
+Vector2dArray* PBody::getVectorArray()
 {
-    return m_position;
+    return m_vectorArray;
 }
-Vector2d* PBody::getSize()
-{
-    return m_size;
-}
+
 bool PBody::isAffectedByGravity()
 {
     return m_affectedByGravity;
 }
 bool PBody::isStationary()
 {
-    return m_stationary;
+    return m_stationaryObject;
 }
-bool PBody::isColliding(PBody* otherBody)
+bool PBody::isCollidingWithBody(PBody* otherBody)
 {
-    if (this->getPosition()->m_x + this->getSize()->m_x - 1 < otherBody->getPosition()->m_x)
-        return false;
-    if (this->getPosition()->m_y > otherBody->getPosition()->m_y + otherBody->getSize()->m_y - 1)
-        return false;
-    if (this->getPosition()->m_x > otherBody->getPosition()->m_x + otherBody->getSize()->m_x - 1)
-        return false;
-    if (this->getPosition()->m_y + this->getSize()->m_y - 1 < otherBody->getPosition()->m_y)
-        return false;
+    float min_x_1 = this->m_vectorArray->m_vectors[0]->m_x;
+    float max_x_1 = this->m_vectorArray->m_vectors[0]->m_x;
+    float min_x_2 = otherBody->m_vectorArray->m_vectors[0]->m_x;
+    float max_x_2 = otherBody->m_vectorArray->m_vectors[0]->m_x;
     
-    return true;
+    float min_y_1 = this->m_vectorArray->m_vectors[0]->m_y;
+    float max_y_1 = this->m_vectorArray->m_vectors[0]->m_y;
+    float min_y_2 = otherBody->m_vectorArray->m_vectors[0]->m_y;
+    float max_y_2 = otherBody->m_vectorArray->m_vectors[0]->m_y;
+    
+    for (int i = 0; i < this->m_vectorArray->m_size; i++) {
+        min_x_1 = min_x_1 > this->m_vectorArray->m_vectors[i]->m_x ? this->m_vectorArray->m_vectors[i]->m_x : min_x_1;
+        max_x_1 = max_x_1 < this->m_vectorArray->m_vectors[i]->m_x ? this->m_vectorArray->m_vectors[i]->m_x : max_x_1;
+        
+        min_y_1 = min_y_1 > this->m_vectorArray->m_vectors[i]->m_y ? this->m_vectorArray->m_vectors[i]->m_y : min_y_1;
+        max_y_1 = max_y_1 < this->m_vectorArray->m_vectors[i]->m_y ? this->m_vectorArray->m_vectors[i]->m_y : max_y_1;
+    }
+        
+    for (int i = 0; i < otherBody->m_vectorArray->m_size; i++) {
+        min_x_2 = min_x_2 > otherBody->m_vectorArray->m_vectors[i]->m_x ? otherBody->m_vectorArray->m_vectors[i]->m_x : min_x_2;
+        max_x_2 = max_x_2 < otherBody->m_vectorArray->m_vectors[i]->m_x ? otherBody->m_vectorArray->m_vectors[i]->m_x : max_x_2;
+        
+        min_y_2 = min_y_2 > otherBody->m_vectorArray->m_vectors[i]->m_y ? otherBody->m_vectorArray->m_vectors[i]->m_y : min_y_2;
+        max_y_2 = max_y_2 < otherBody->m_vectorArray->m_vectors[i]->m_y ? otherBody->m_vectorArray->m_vectors[i]->m_y : max_y_2;
+    }
+    
+//    Log(LOG_INFO, "PBody", generateCString("Movement: %d,%d", m_movementVector->m_x, m_movementVector->m_y));
+    Log(LOG_INFO, "PBody", generateCString("x1: %d,%d y1: %d,%d", min_x_1, max_x_1, min_y_1, max_y_1));
+    Log(LOG_INFO, "PBody", generateCString("x2: %d,%d y2: %d,%d", min_x_2, max_x_2, min_y_2, max_y_2));
+    
+    if (!((min_x_1 < min_x_2 && max_x_1 < min_x_2) || (min_x_2 < min_x_1 && max_x_2 < min_x_1)))
+        if (!((min_y_1 < min_y_2 && max_y_1 < min_y_2) || (min_y_2 < min_y_1 && max_y_2 < min_y_1))) {
+            Log(LOG_INFO, "Collision", "True!");
+            return true;
+        }
+    Log(LOG_INFO, "Collision", "False!");
+    return false;
 }
-bool PBody::intersectionWithPoint(Vector2d* point)
+PBodyType PBody::getTag()
 {
-    if (point->m_x < this->getPosition()->m_x)
-        return false;
-    if (point->m_y < this->getPosition()->m_y)
-        return false;
-    if (point->m_x > this->getPosition()->m_x + this->getSize()->m_x - 1)
-        return false;
-    if (point->m_y > this->getPosition()->m_y + this->getSize()->m_y - 1)
-        return false;
-    
-    return true;
+    return m_tag;
 }
