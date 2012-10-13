5bca9d097a65f59a4de4e591dd43cf4a5cb67f6f
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-10-13 13:58:52 +0200

    Added a new constructor for non tetragonic shapes.

--- src/View/Sprite.cpp
+++ src/View/Sprite.cpp
@@ -16,9 +16,9 @@ Sprite::Sprite(float x, float y, float w, float h, int textureID)
 {
     m_vertex = new const Vertex[4] {
         Vertex(x        , y + h),
-        Vertex(x + w    , y + h),
         Vertex(x        , y),
         Vertex(x + w    , y),
+        Vertex(x + w    , y + h),
     };
     
     m_textureID = textureID;
@@ -27,6 +27,11 @@ Sprite::Sprite(Sprite* sprite)
 {
     m_vertex = sprite->m_vertex;
     m_textureID = sprite->m_textureID;
+}
+Sprite::Sprite(Vertex* vertecies, int num_vertecies, int textureID)
+{
+    m_vertex = new const Vertex(vertecies);
+    m_textureID = textureID;
 }
 
 Sprite::~Sprite()
