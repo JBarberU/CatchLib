944cb91487dfe5e57851de237777f03e9e057cb6
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-10-14 14:17:02 +0200

    Added a function to calculate absolute values.
    Simple, but nice to have in one place.

--- src/Math/Math.cpp
+++ src/Math/Math.cpp
@@ -14,8 +14,8 @@
 
 Vector2d* Math::generateUnitVectorOf(Vector2d* vector)
 {
-    if (vector->m_x == 0 && vector->m_y == 0) {
-        return new Vector2d(0,0);
+    if (vector == 0 || (vector->m_x == 0 && vector->m_y == 0)) {
+        return new Vector2d(0.0,0.0);
     }
     
     float scalar = sqrtf((vector->m_x * vector->m_x) + (vector->m_y * vector->m_y));
@@ -23,4 +23,9 @@ Vector2d* Math::generateUnitVectorOf(Vector2d* vector)
     Vector2d* returnVector = new Vector2d(vector->m_x/scalar, vector->m_y/scalar);
     
     return returnVector;
+}
+
+double Math::abs(double value)
+{
+    return value > 0 ? value : -1 * value;
 }
