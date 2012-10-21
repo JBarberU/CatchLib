6bc3a4070231907e853ec49854154c3686102dcc
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-10-21 19:18:20 +0200

    Made the GameController utilize the new constants class.
    The constants keeps track of values such as game coordinates.

--- src/Controller/GameController.cpp
+++ src/Controller/GameController.cpp
@@ -12,19 +12,19 @@
 #include "GameController.hpp"
 #include "../Helper/Logger.hpp"
 #include "../Helper/InputManager.hpp"
+#include "../Helper/Constants.hpp"
 
 
 GameController::GameController(int width, int height, CLTexture* texture)
 {
 	srand ( time(NULL) );
 
+    Constants::init(width, height);
+    
     m_deviceWidth = width;
     m_deviceHeight = height;
     
     m_deviceOrientation = DeviceOrientationLandscapeLeft;
-    
-    Log(LOG_INFO, "GameController", generateCString("GameCon: %ix%i", width, height));
-    
     InputManager::getSharedManager()->addInputListener(this);
 
     m_renderer = CreateRendererWithOpenGL10();
