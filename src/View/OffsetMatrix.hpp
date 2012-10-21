65cb4d3287324fc87d31240d08c9acd0444f2aa3
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-10-21 20:19:39 +0200

    Created a class to keep track of the offset for each corner of a sprite.
    This is mainly used by the Actor class.

---
+++ src/View/OffsetMatrix.hpp
@@ -0,0 +1,33 @@
+//
+//  OffsetMatrix.hpp
+//  CatchiOS
+//
+//  Created by John Barbero Unenge on 10/15/12.
+//  Copyright (c) 2012 John Barbero Unenge. All rights reserved.
+//
+
+#ifndef CatchiOS_OffsetMatrix_hpp
+#define CatchiOS_OffsetMatrix_hpp
+
+#include "../Math/Vector2d.hpp"
+
+struct OffsetMatrix {
+    Vector2d* m_bottomLeft;
+    Vector2d* m_bottomRight;
+    Vector2d* m_topLeft;
+    Vector2d* m_topRight;
+    OffsetMatrix(Vector2d* bottomLeft, Vector2d* bottomRight, Vector2d* topLeft, Vector2d* topRight) {
+        m_bottomLeft = bottomLeft;
+        m_bottomRight = bottomRight;
+        m_topLeft = topLeft;
+        m_topRight = topRight;
+    }
+    OffsetMatrix(OffsetMatrix* matrix) {
+        this->m_bottomLeft = matrix->m_bottomLeft;
+        this->m_bottomRight = matrix->m_bottomRight;
+        this->m_topLeft = matrix->m_topLeft;
+        this->m_topRight = matrix->m_topRight;
+    }
+};
+
+#endif
