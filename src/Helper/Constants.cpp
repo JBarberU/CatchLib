70f85cba24a262ee08652fce0cec39a15dd23d46
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-10-21 20:17:37 +0200

    Created a constants class.
    This will keep track of global values that are useful to other parts
    of the application.

---
+++ src/Helper/Constants.cpp
@@ -0,0 +1,30 @@
+//
+//  File:	Constants.cpp
+//  Author: John Barbero Unenge 
+//	Date:  	10/20/12.
+//
+//  Copyright (c) 2012 John Barbero Unenge. All rights reserved.
+//
+//	All code is my own except where credited to others.
+//
+
+
+#include "Constants.hpp"
+
+double Constants::gameWidth;
+double Constants::gameHeight;
+
+void Constants::init(double screenWidth, double screenHeight)
+{
+    gameWidth = 18;
+    gameHeight = (screenWidth / screenHeight) * gameWidth;
+}
+
+double Constants::getGameWidth()
+{
+    return gameWidth;
+}
+double Constants::getGameHeight()
+{
+    return gameHeight;
+}
