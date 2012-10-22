f6d0045fb45e23936cb11867607dee73c0844aa5
Jesper Persson <JesperT.Persson@gmail.com>
2012-10-22 23:00:31 +0200

    Chainsaw removed when colliding with player

--- src/GameModel/GameModel.cpp
+++ src/GameModel/GameModel.cpp
@@ -50,15 +50,25 @@ void GameModel::playerJump()
 }
 void GameModel::playerThrowAt(int x, int y)
 {
-    double convertedX = x * Constants::getGameWidth();
-    double convertedY = y * Constants::getGameHeight();
+    if (m_chainsaw == 0){
+        double convertedX = x * Constants::getGameWidth();
+        double convertedY = y * Constants::getGameHeight();
                         
-    b2Vec2 target = b2Vec2( convertedX + m_player->getPosition().x, convertedY + m_player->getPosition().y);
-    b2Vec2 spawn = b2Vec2(m_player->getPosition().x + 3, m_player->getPosition().y + 3);
-    m_chainsaw = new Chainsaw(b2Vec2(1.0, 1.0 ),spawn , target, false, true, PB_PLAYER);
-    Log(LOG_INFO, "GameModel", generateCString("Player threw his chainsaw at: %i,%i", x, y));
+        b2Vec2 target = b2Vec2( convertedX + m_player->getPosition().x, convertedY + m_player->getPosition().y);
+        b2Vec2 spawn = b2Vec2(m_player->getPosition().x + 3, m_player->getPosition().y + 3);
+    
+        m_chainsaw = new Chainsaw(b2Vec2(1.0, 1.0 ),spawn , target, false, true, PB_PLAYER);
+        Log(LOG_INFO, "GameModel", generateCString("Player threw his chainsaw at: %i,%i", x, y));
+    }
 }
 
+void GameModel::onEvent(EEvent event, void* source)
+{
+    if (event == COLLIDED_WITH_PLAYER && ((PBody *)source)->getTag() == PB_CHAINSAW){
+        delete m_chainsaw;
+        m_chainsaw = 0;
+    }
+}
 void GameModel::onEvent(EEvent event, void* source)
 {
 	if(event == COLLIDED_WITH_PLAYER && ((PBody *)source)->getTag() == PB_CHAINSAW){
