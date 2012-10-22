8185682585921f8c6e938893cf43fc7bf2677bda
Jesper Persson <JesperT.Persson@gmail.com>
2012-10-22 13:34:37 +0200

    Added implementation-file for chainsaw. Only documented as of now.

---
+++ src/GameModel/Entities/Chainsaw.cpp
@@ -0,0 +1,11 @@
+//
+//  File:       EventBus.hpp
+//  Class:      EventBus
+//  Author:     Jesper Persson and Sebastian Odbjer
+//              All code is our own except where credited to others.
+//
+//	Copyright (c) 2012 by Catch22. All Rights Reserved.
+//  Date: 		02/10-2012
+//
+//	License: The following code is licensed under the Catch22-License
+//
