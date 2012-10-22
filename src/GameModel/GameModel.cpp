f505a4b9e4a9b944c4a8b88c59bce4d7fa95bda8
Jesper Persson <JesperT.Persson@gmail.com>
2012-10-22 23:43:15 +0200

    Chainsaw no longer returns after target is reached

--- src/GameModel/GameModel.cpp
+++ src/GameModel/GameModel.cpp
@@ -29,11 +29,11 @@ GameModel::~GameModel()
 void GameModel::update(float dt)
 {
     if (m_chainsaw != 0){
-        if (m_chainsaw->targetReached()){
-            m_chainsaw->setTarget(m_player->getPosition());
-            Log(LOG_INFO, "Chainsaw", "The Chainsaw has reached its target!!  ****");
-        }
         m_chainsaw->update(dt);
+        if (m_chainsaw->targetReached()){
+            //m_chainsaw->setTarget(m_player->getPosition());
+            m_chainsaw = 0;
+        }
     }
     m_physicsManager->update(dt);
     m_player->update();
@@ -51,14 +51,10 @@ void GameModel::playerJump()
 void GameModel::playerThrowAt(int x, int y)
 {
     if (m_chainsaw == 0){
-        double convertedX = x * Constants::getGameWidth();
-        double convertedY = y * Constants::getGameHeight();
                         
-        b2Vec2 target = b2Vec2( convertedX + m_player->getPosition().x, convertedY + m_player->getPosition().y);
-        b2Vec2 spawn = b2Vec2(m_player->getPosition().x + 3, m_player->getPosition().y + 3);
+        b2Vec2 spawn = b2Vec2(m_player->getPosition().x, m_player->getPosition().y);
     
-        m_chainsaw = new Chainsaw(b2Vec2(1.0, 1.0 ),spawn , target, false, true, PB_PLAYER);
-        Log(LOG_INFO, "GameModel", generateCString("Player threw his chainsaw at: %i,%i", x, y));
+        m_chainsaw = new Chainsaw(b2Vec2(1.0, 1.0 ),spawn , b2Vec2(x,y), false, true, PB_PLAYER);
     }
 }
 
